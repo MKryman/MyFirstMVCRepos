1a2b253234be6903e61b2afc9da838b5148a8ce7
Malky Kryman <malkykryman@gmail.com>
2023-05-08 00:12:19 -0400

    Add project files.

---
+++ Classwork_03-27(setting up git)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Classwork_03_27_setting_up_git_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
